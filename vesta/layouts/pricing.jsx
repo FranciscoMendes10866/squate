4f14e4399ab5b3e6b7518927556d9972fef6562e
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2021-04-23 20:34:00 +0100

    pricing section done

--- vesta/layouts/pricing.jsx
+++ vesta/layouts/pricing.jsx
@@ -5,34 +5,31 @@ const Pricing = () => {
         <div className="columns is-centered">
           <div className="column is-8">
             <h1 className="is-size-3 has-text-left-mobile has-text-weight-semibold has-text-dark">
-              Pricing
+              Os meus serviços
             </h1>
             <p className="is-size-5 mt-5 mb-6 has-text-left-mobile has-text-grey">
-              The platform is completely free. Start enjoying it right now.
+              Eu quero que prove a si mesma do que é feita - e que tire o melhor de si e do seu corpo.
             </p>
-            <div className="card remove-shadow has-background-white-bis">
+            <div className="card remove-shadow has-background-white-bis py-5">
               <div className="card-content">
                 <div className="content">
                   <div className="columns is-align-items-center">
                     <div className="column">
                       <ul className="has-text-grey has-text-left reset">
                         <li className="ok-hand">
-                          <small>Unlimited links</small>
+                          <small>HIT</small>
                         </li>
                         <li className="ok-hand">
-                          <small>Unlimited seats</small>
+                          <small>1:1 Personal Training</small>
                         </li>
                         <li className="ok-hand">
-                          <small>Redirect unlimited links</small>
+                          <small>Personal Training DUO</small>
                         </li>
                         <li className="ok-hand">
-                          <small>Custom Slug</small>
+                          <small>Small Group Coaching</small>
                         </li>
                         <li className="ok-hand">
-                          <small>Number of clicks allowed</small>
-                        </li>
-                        <li className="ok-hand">
-                          <small>SSL protection</small>
+                          <small>Online Training</small>
                         </li>
                       </ul>
                     </div>
@@ -40,15 +37,15 @@ const Pricing = () => {
                       <div className="is-flex is-flex-direction-column">
                         <h1 className="is-size-1 has-text-grey has-text-weight-light mb-5">
                           <span className="is-size-1 has-text-dark has-text-weight-semibold">
-                            € 0
+                            € ?
                           </span>
-                          /mo
+                          /mês
                         </h1>
                         <button className="button is-link mb-4 is-medium">
-                          Get Started for Free
+                          Contactar agora!
                         </button>
                         <small className="has-text-grey-light sm-txt">
-                          No credit card required.
+                          Mande-me uma mensagem para saber mais.
                         </small>
                       </div>
                     </div>
